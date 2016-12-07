7db3accbd0635195e96b8ac24d60926685291152
Steven Roper <steven.m.roper@gmail.com>
2016-12-07 15:44:58 -0700

    Create search form for search container.

--- src/containers/search_container.jsx
+++ src/containers/search_container.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router';
 
 class SearchContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  handleInputChange(e) {
+    this.setState({ term: e.target.value });
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div>
@@ -10,9 +27,18 @@ class SearchContainer extends Component {
           label="< Back"
           secondary={true}
           containerElement={<Link to='/' />} />
-        Search Container
+        <form className="search-form" onSubmit={this.handleFormSubmit.bind(this)}>
+          <TextField
+            floatingLabelText="Search for artists, albums, etc..."
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.term} />
+          <RaisedButton
+            label="Search"
+            secondary={true}
+            type="submit" />
+        </form>
       </div>
-    );  
+    );
   }
 }
 
